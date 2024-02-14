64e83b2abf1b075fa5edf07df5d511506832e3c5
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-14 16:41:27 +0200

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,11 @@
--- a script that creates the database hbtn_0d_2 and the user user_0d_2.
--- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
--- The user_0d_2 password should be set to user_0d_2_pwd
--- If the database hbtn_0d_2 already exists, your script should not fail
--- If the user user_0d_2 already exists, your script should not fail
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER
-       IF NOT EXISTS 'user_0d_2'@'localhost'
-       IDENTIFIED BY 'User_0d_2_pwd@@';
-GRANT SELECT
-      ON hbtn_0d_2.*
-      TO user_0d_2@localhost;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the hbtn_0d_2 database to the user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
