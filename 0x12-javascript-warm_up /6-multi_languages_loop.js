7af132f1aad225e5ec7b7cbffbba2d492d432914
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-12 09:18:44 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up /6-multi_languages_loop.js
+++ 0x12-javascript-warm_up /6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(line);
+for (const message of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(message);
 }
