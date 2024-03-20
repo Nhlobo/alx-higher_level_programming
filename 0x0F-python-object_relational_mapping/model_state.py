73da2b0ec606b09f11480f6cbc87e39490ee9e0b
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-20 16:32:43 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
+"""Define a State class and create a connection to the database.
 """
-Defines a state model that contain the class definition
- of a State and an instance Base = declarative_base()
-"""
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
-from sqlalchemy import Column, Integer, String
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+"""Create an instance of declarative base"""
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    inherits from Base Tips
-    links to the MySQL table states
-    class attribute id that represents a column
-     of an auto-generated, unique integer, can't
-      be null and is a primary key
-    class attribute name that represents a column
-     of a string with maximum 128 characters and
-      can't be null
+    """Define the State class"""
+    __tablename__ = 'states'
 
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
