e5ff55880f012f679db3c2335124ca3385a489bc
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-27 18:02:49 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a POST request to the passed URL,
-# and displays the body of the response. It sends variables email and subject with predefined values.
-
-URL=$1
-
-# Send a POST request to the URL with the specified parameters
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$URL"
+# sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
