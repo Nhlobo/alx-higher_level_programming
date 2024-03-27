f7a7fbd0b73932857c9b62e744192fa98e7f409f
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-27 17:58:30 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response if the status code is 200
-
-URL=$1
-
-# Send a GET request to the URL and display the body if the status code is 200
-curl -s -X GET -L -w "%{http_code}" "$URL" -o /dev/null | 
-if [ $(cat -) -eq 200 ]; then
-    curl -s "$URL"
-fi
+# sends a GET request to an URL, and displays the body of the response
+curl -sL "$1"
