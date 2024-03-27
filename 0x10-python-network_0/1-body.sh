62100d9073d1f138cf0a45f5bfc430eeeb956d6c
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-27 17:42:16 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# This script takes in a URL, sends a GET request to the URL,
+# and displays the body of the response if the status code is 200
 
 URL=$1
 
-# Send a request to the URL and get the body size using curl
-SIZE=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}')
-
-echo "$SIZE"
+# Send a GET request to the URL and display the body if the status code is 200
+curl -s -X GET -L -w "%{http_code}" "$URL" -o /dev/null | 
+if [ $(cat -) -eq 200 ]; then
+    curl -s "$URL"
+fi
