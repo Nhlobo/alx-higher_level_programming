b4322f768c4f791d28f17e410233b3825731283a
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-27 17:38:05 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# This script takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+
+URL=$1
+
+# Send a request to the URL and get the body size using curl
+SIZE=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}')
+
+echo "$SIZE"
