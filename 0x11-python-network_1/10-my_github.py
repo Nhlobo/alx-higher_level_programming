77b0534349c801ec04e62577d460273c86fb3d4b
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-28 08:56:38 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+"""
+Module to use the GitHub API to display the user's id using Basic Authentication with a personal access token.
+"""
 
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    Takes in the GitHub username and personal access token as arguments, and uses the GitHub API to display the user's id.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        data = response.json()
+        print(data.get('id'))
+    except ValueError:
+        print("None")
