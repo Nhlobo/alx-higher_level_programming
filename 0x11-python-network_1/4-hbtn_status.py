a77537412907c19000fe8567eb90fc5333feb388
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-28 08:41:00 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""
+Module to fetch https://alx-intranet.hbtn.io/status using the requests package.
+"""
 
+import requests
+
+if __name__ == "__main__":
+    """
+    Fetches https://alx-intranet.hbtn.io/status and displays the body response.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
