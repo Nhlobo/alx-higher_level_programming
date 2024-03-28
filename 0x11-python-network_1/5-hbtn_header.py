fe0fbce11952dd383f5436f78a288bf25886cc9c
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-28 08:45:09 +0200

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+Module to fetch the value of the X-Request-Id variable from the header of the response using the requests package.
+"""
 
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    Takes in a URL, sends a request to the URL, and displays the value of the X-Request-Id variable found in the header of the response.
+    """
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
