d0ec96cbf426cf16516f7ea779fc1fe5811226fd
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-28 15:37:39 +0200

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Send request to URL .
+awk 'NR==1{printf "%s", $2}' headers "$(curl -sI "$1" -o headers)"
