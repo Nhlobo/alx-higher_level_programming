818ce1460e69f8ccff273701412cf6a02c95e8c2
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-28 08:54:16 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to send a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter.
+"""
 
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    Takes in a letter, sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter, and displays the response.
+    If the response body is properly JSON formatted and not empty, displays the id and name.
+    Otherwise, displays "Not a valid JSON" if the JSON is invalid, and "No result" if the JSON is empty.
+    """
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
