c5bc1da5ea288c1b17ca8eeb9b6511954c7b95f9
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-29 08:17:33 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-Module to fetch https://alx-intranet.hbtn.io/status
+Module to fetch https://intranet.hbtn.io/status
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    """
-    Fetches https://alx-intranet.hbtn.io/status and displays the body response.
-    """
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
