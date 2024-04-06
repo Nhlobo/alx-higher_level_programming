0d6bdc7bc37d67d48ce73022f169630c614fb092
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-04-06 23:35:50 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up /7-multi_c.js
+++ 0x12-javascript-warm_up /7-multi_c.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-const x = Number(process.argv[2]);
-if (x) {
+
+const process = require('process');
+const firstArg = process.argv[2];
+
+const x = parseInt(firstArg);
+
+if (!isNaN(x)) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
