c48097d5866d82c423ee7d8254c816d752bf1e0b
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-04-06 23:27:36 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up /10-factorial.js
+++ 0x12-javascript-warm_up /10-factorial.js
@@ -1,8 +1,21 @@
 #!/usr/bin/node
-function factorial (nbr) {
-  if (!nbr || nbr < 2) {
-    return (1);
+
+const process = require('process');
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+function calcFactorial (x) {
+  if (isNaN(x)) {
+    return 1;
+  } else if (x === 0) {
+    return 1;
+  } else {
+    return x * calcFactorial(x - 1);
   }
-  return nbr * factorial(nbr - 1);
 }
-console.log(factorial(Number(process.argv[2])));
+
+if (!isNaN(num)) {
+  console.log(calcFactorial(num));
+} else {
+  console.log('1');
+}
