af60082f3c994fd341b15a2e26c88fd0e49b6e9b
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-04-06 23:34:20 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up /5-to_integer.js
+++ 0x12-javascript-warm_up /5-to_integer.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (Number(process.argv[2])) {
-  console.log(`My number: ${Number(process.argv[2])}`);
+
+const process = require('process');
+const firstArg = process.argv[2];
+const convertedFirstArg = parseInt(firstArg);
+
+if (!isNaN(convertedFirstArg)) {
+  console.log('My number:', convertedFirstArg);
 } else {
   console.log('Not a number');
 }
