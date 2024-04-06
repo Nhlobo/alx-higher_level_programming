c7e9116d9cda7362c6823e17634789421028c42c
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-04-06 23:32:14 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up /2-arguments.js
+++ 0x12-javascript-warm_up /2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
+
+const argumentsCount = process.argv.length - 2;
+
+if (argumentsCount === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (argumentsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
