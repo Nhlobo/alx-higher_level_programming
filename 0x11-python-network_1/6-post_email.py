5ea02640b7508d513171d23499940cba438a69ce
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-04-06 13:31:16 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-Module to send a POST request with an email as a parameter and display the body of the response using the requests package.
-"""
+"""using requests module to fetch"""
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    """
-    Takes in a URL and an email, sends a POST request to the URL with the email as a parameter, and displays the body of the response.
-    """
+    # get the url from the command line
     url = sys.argv[1]
     email = sys.argv[2]
 
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
+    # create a dictionary with the email parameter
+    data = {'email': email}
+
+    # send a POST request to the URL
+    response = requests.post(url, data=data)
     print(response.text)
